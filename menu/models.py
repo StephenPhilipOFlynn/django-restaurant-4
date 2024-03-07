a9452e17db7dd93696683c0316153bf79046402f
Stephen OFlynn <soflynn@tcd.ie>
2024-03-07 17:11:12 +0000

    update admin.py file to upload meals, upload meals to django admin

--- menu/models.py
+++ menu/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Meal(models.Model):
-    name = models.CharField(max_length = 50)
-    description = models.Textfield()
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     picture = models.ImageField(upload_to="menu/")
+
+def __str__(self):
+    return self.name
