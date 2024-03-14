f2758a45b7b4005e88aaf011768ff119358ca063
Stephen OFlynn <soflynn@tcd.ie>
2024-03-14 16:47:52 +0000

    create model for restaurant history and blurb

--- ourrestaurant/models.py
+++ ourrestaurant/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class OurRestaurant(models.Model):
+    title = models.CharField(max_length = 50)
+    content = models.TextField()
+    image = models.ImageField(upload_to="ourrestaurant/")
+
+#consider whether other models here to fill out restaurant history
 
