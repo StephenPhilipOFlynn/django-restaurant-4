8b1a6e0e1af4cf64ca87712bcfb3efc109f6ad56
Stephen OFlynn <soflynn@tcd.ie>
2024-03-18 17:16:01 +0000

    set up pathway to template in views.py file of menu app

--- menu/views.py
+++ menu/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import Meal
+
 def menu_list(request):
-    pass
+    menu_list = Meal.objects.all()
+    context = {'menu_list': menu_list}
+    return render(request , 'menu/templates/list.html', context)
