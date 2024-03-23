d9fb3ae1c56b68d5295466b2b1e7e99a70ec42e7
Stephen OFlynn <soflynn@tcd.ie>
2024-03-23 03:47:22 +0000

    create chefs model in our restaurant app

--- ourrestaurant/models.py
+++ ourrestaurant/models.py
@@ -9,5 +9,11 @@ class OurRestaurant(models.Model):
     def __str__(self):
         return self.title
 
-#consider whether other models here to fill out restaurant history
+class ChefsInfo(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    image = models.ImageField(upload_to="ourrestaurant/")
+
+    def __str__(self):
+        return self.name
 
