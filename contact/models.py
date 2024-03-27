f95b5e72fd449d982b10fb54cebc993463305377
Stephen OFlynn <soflynn@tcd.ie>
2024-03-27 20:30:35 +0000

    adjust model for contact message to record time of message

--- contact/models.py
+++ contact/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Contact(models.Model):
@@ -6,6 +7,10 @@ class Contact(models.Model):
     subject = models.CharField(max_length=100, help_text="Enter your subject")
     email = models.EmailField(help_text="Enter your email address")
     message = models.CharField(max_length=500, help_text="Enter your message")
+    received_at = models.DateTimeField(auto_now_add=True, help_text="Message received at")
+
+    class Meta:
+        verbose_name_plural = "Contact"
 
     def __str__(self):
         return self.name
