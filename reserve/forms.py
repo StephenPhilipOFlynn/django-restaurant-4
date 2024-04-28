dffc19c4f8dd72922ffc23d1ce08caad28bdb4e8
Stephen OFlynn <soflynn@tcd.ie>
2024-04-28 17:36:04 +0000

    adjust forms.py to enforce min guests 1, max guests 8

--- reserve/forms.py
+++ reserve/forms.py
@@ -3,6 +3,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from .models import TableReservation
 
 class ReserveForm(forms.ModelForm):
+    # Enforce number of guests to be between 1 and 8 on client side of form
+    number_of_guests = forms.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(8)],
+        widget=forms.NumberInput(attrs={'class': 'reserve-form-guests', 'min': 1, 'max': 8})
+    )
+
     class Meta:
         model = TableReservation
         fields = ['name', 'email', 'phone', 'requests', 'number_of_guests', 'date_and_time']
@@ -11,8 +17,7 @@ class ReserveForm(forms.ModelForm):
             'email': forms.EmailInput(attrs={'class': 'reserve-form-email'}),
             'phone': forms.TextInput(attrs={'class': 'reserve-form-phone'}),
             'requests': forms.Textarea(attrs={'class': 'reserve-form-requests'}),
-            'number_of_guests': forms.NumberInput(attrs={'class': 'reserve-form-guests'}),
-            'date_and_time': forms.DateTimeInput(attrs={'class': 'reserve-form-date-and-time', 'type': 'datetime-local', 'step': '1800'}),
+            'date_and_time': forms.DateTimeInput(attrs={'class': 'reserve-form-date-and-time', 'type': 'datetime-local', 'step': '1800', 'id': 'reserve_date_and_time'}),
         }
         labels = {
             'name': 'Name',
