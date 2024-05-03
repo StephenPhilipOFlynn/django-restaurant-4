3ff7daf9e9a2a5bd91b81842a6aee72a214ca3fc
Stephen OFlynn <soflynn@tcd.ie>
2024-05-03 05:03:16 +0000

    test website image files in static folder

--- my_project/settings.py
+++ my_project/settings.py
@@ -141,7 +141,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media")
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
