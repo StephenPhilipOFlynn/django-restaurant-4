7b92de4331ff81feabefe39e346fb9baf5297ed1
Stephen "O'Flynn <soflynn@tcd.ie>
2024-07-14 16:39:51 +0000

    update CSRF trusted origins, and update allowed hosts

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iuo_74_b%l6-_f!01@ky$qrwfd4-p#wgv@r8kh@yc!883u^4%_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-stephenphil-djangoresta-5vnxpm6o8o1.ws-eu110.gitpod.io', '8000-stephenphil-djangoresta-5vnxpm6o8o1.ws-us110.gitpod.io', '8000-stephenphil-djangoresta-80pnyq9b1ib.ws-eu110.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-stephenphil-djangoresta-4m5hepdo2ap.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
@@ -98,9 +98,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://8000-stephenphil-djangoresta-5vnxpm6o8o1.ws-us108.gitpod.io",
-    "https://8000-stephenphil-djangoresta-5vnxpm6o8o1.ws-us110.gitpod.io",
-    "https://8000-stephenphil-djangoresta-5vnxpm6o8o1.ws-eu110.gitpod.io", "https://8000-stephenphil-djangoresta-80pnyq9b1ib.ws-eu110.gitpod.io",
+    "https://8000-stephenphil-djangoresta-4m5hepdo2ap.ws.codeinstitute-ide.net/",
     "https://*.herokuapp.com"
 ]
 
