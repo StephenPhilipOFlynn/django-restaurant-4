d77267df7794a61ae0d1116a3ed291b8cf487834
Stephen "O'Flynn <soflynn@tcd.ie>
2024-07-19 11:11:43 +0000

    update menu model

--- menu/models.py
+++ menu/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
-
-# Create your models here.
 class Meal(models.Model):
+    STARTER = 'ST'
+    MAIN_COURSE = 'MC'
+    DESSERT = 'DS'
+    DRINK = 'DR'
+    CATEGORY_CHOICES = [
+        (STARTER, 'Starter'),
+        (MAIN_COURSE, 'Main Course'),
+        (DESSERT, 'Dessert'),
+        (DRINK, 'Drink'),
+    ]
+
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     picture = models.ImageField(upload_to="menu/")
+    category = models.CharField(
+        max_length=2,
+        choices=CATEGORY_CHOICES,
+        default=MAIN_COURSE,
+    )
     
     def __str__(self):
         return self.name
