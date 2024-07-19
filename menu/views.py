388ff04cb2abe3f8ac4437b6db850c51e643b72f
Stephen "O'Flynn <soflynn@tcd.ie>
2024-07-19 14:44:32 +0000

    update menu views

--- menu/views.py
+++ menu/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 from .models import Meal
 
-def menu_list(request):
-    menu_list = Meal.objects.all()
-    context = {'menu_list': menu_list}
-    return render(request, 'menu/list.html', context)
+def menu_view(request):
+    starters = Meal.objects.filter(category=Meal.STARTER)
+    main_courses = Meal.objects.filter(category=Meal.MAIN_COURSE)
+    desserts = Meal.objects.filter(category=Meal.DESSERT)
+    drinks = Meal.objects.filter(category=Meal.DRINK)
+   
+    context = {
+        'starters': starters,
+        'main_courses': main_courses,
+        'desserts': desserts,
+        'drinks': drinks,
+    }
+    return render(request, 'menu.html', context)
