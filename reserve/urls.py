d480c4db0f622b26fd8304e4dee8a20d7b94cd23
Stephen "O'Flynn <soflynn@tcd.ie>
2024-07-20 14:04:42 +0000

    adjust urls.py, views.py in reserve app to get success view, check registration in master urls.py file

--- reserve/urls.py
+++ reserve/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'reserve'
 
 urlpatterns = [
-    path('', views.book_table, name='book_table'),]
+    path('', views.book_table, name='book_table'),
+    path('success/', views.reservation_success, name='reservation_success'),
+]
--- reserve/views.py
+++ reserve/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 from .models import TableReservation
 from .forms import ReserveForm
+
+def reservation_success(request):
+    return render(request, 'Reserve/success.html')
 
 def book_table(request):
     reserve_form = ReserveForm()
@@ -18,6 +21,3 @@ def book_table(request):
     context = {'form': reserve_form}
     
     return render(request, 'Reserve/reserve.html' , context)
-
-def reservation_success(request):
-    return render(request, 'Reserve/success.html')
