cfb52c8b912bd0905c5c729b67c5c4006d4d4a6c
Stephen "O'Flynn <soflynn@tcd.ie>
2024-07-21 21:21:26 +0000

    create hmtl templates for login, profile, delete, signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import AccountUserCreationForm, AccountAuthenticationForm
+from reserve.models import TableReservation
 
 # Create your views here.
+
