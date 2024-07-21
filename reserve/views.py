e1b57c3419f8efb31580a4d245569e325002b444
Stephen "O'Flynn <soflynn@tcd.ie>
2024-07-21 10:31:41 +0000

    update reserve.html to display errors, correct models.py logic for incorrect bookings, correct view for incorrect booking

--- reserve/views.py
+++ reserve/views.py
@@ -9,15 +9,16 @@ def reservation_success(request):
     return render(request, 'Reserve/success.html')
 
 def book_table(request):
-    reserve_form = ReserveForm()
-
-    if request.method == 'POST':
+    if request.method == 'POST':  
         reserve_form = ReserveForm(request.POST)
+        if reserve_form.is_valid():  
+            reserve_form.save()  
+            return redirect('reserve:reservation_success')  
+        else:
+            context = {'form': reserve_form}  
+            return render(request, 'Reserve/reserve.html', context)
+    else:  
+        reserve_form = ReserveForm()  
 
-        if reserve_form.is_valid():
-            reserve_form.save()
-            return redirect('reserve:reservation_success')
-            
-    context = {'form': reserve_form}
-    
-    return render(request, 'Reserve/reserve.html' , context)
+    context = {'form': reserve_form} 
+    return render(request, 'Reserve/reserve.html', context)  
